a2ffafdd0f51cecc93ac028d665d4babbc108f85
Loup Menjoulet <lmenjoulet@gmail.com>
2022-03-01 23:56:42 +0900

    decoupled languagePicker logic and it's visible state

--- src/components/LanguagePicker.svelte
+++ src/components/LanguagePicker.svelte
@@ -13,19 +13,17 @@
 
 </script>
 
-{#if path.match(new RegExp(`^(\/${lang}/)`)) }
-    <div>
-        <select name="languages" id="lang" 
-            bind:value={lang} 
-            on:change={onLanguageSelected}
-            class="p-2 text-amber-100 bg-stone-900 rounded border-2 border-stone-800 outline-none shadow-md shadow-black/50"    
-        >
-            {#each locales.locales as locale}
-                <option value={locale.locale} class="hover:bg-none">
-                    {locale.language}
-                </option>
-            {/each}
-        </select>
-    </div>
-{/if}
+<div>
+    <select name="languages" id="lang" 
+        bind:value={lang} 
+        on:change={onLanguageSelected}
+        class="p-2 text-amber-100 bg-stone-900 rounded border-2 border-stone-800 outline-none shadow-md shadow-black/50"    
+    >
+        {#each locales.locales as locale}
+            <option value={locale.locale} class="hover:bg-none">
+                {locale.language}
+            </option>
+        {/each}
+    </select>
+</div>
 
